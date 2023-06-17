771a29ecf739d7e887b93daff924b8f66856cbca
dongguo <291320608@qq.com>
2023-06-17 20:50:42 +0800

    多环境切换

---
+++ springboot3-core/src/main/java/com/dongguo/springboot3/core/Springboot3CoreApplication.java
@@ -0,0 +1,13 @@
+package com.dongguo.springboot3.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3CoreApplication.class, args);
+	}
+
+}
