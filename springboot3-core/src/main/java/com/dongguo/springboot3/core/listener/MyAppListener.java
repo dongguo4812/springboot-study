aed8090b83b4633083d163fc58b6cc3c1146f535
dongguo <291320608@qq.com>
2023-06-17 20:49:58 +0800

    多环境切换

---
+++ springboot3-core/src/main/java/com/dongguo/springboot3/core/listener/MyAppListener.java
@@ -0,0 +1,48 @@
+import org.springframework.boot.ConfigurableBootstrapContext;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.time.Duration;
+
+/**
+ * @author dongguo
+ * @date 2023/6/17
+ * @description:
+ */
+public class MyAppListener implements SpringApplicationRunListener {
+    @Override
+    public void starting(ConfigurableBootstrapContext bootstrapContext) {
+        System.out.println("=========start======正在启动================");
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableBootstrapContext bootstrapContext, ConfigurableEnvironment environment) {
+        System.out.println("=========environmentPrepared======环境准备完成================");
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+        System.out.println("=========contextPrepared======IOC容器准备完成================");
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+        System.out.println("=========contextLoaded======IOC容器加载完成================");
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context, Duration timeTaken) {
+        System.out.println("=========started=====启动完成================");
+    }
+
+    @Override
+    public void ready(ConfigurableApplicationContext context, Duration timeTaken) {
+        System.out.println("=========ready=====准备完成================");
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+        System.out.println("=========failed=====启动失败================");
+    }
+}
---
+++ springboot3-core/src/test/java/com/dongguo/springboot3/core/Springboot3CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongguo.springboot3.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
